7fb9be923c04ce37a7014d878c3caa54e784cffc
Vinícius Toshio <vtoshio2004@gmail.com>
2025-09-30 23:10:39 -0300

    chore: adicionar models, relacionamentos e validacoes basicas

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  belongs_to :creator, class_name: "User", foreign_key: :creator_id, inverse_of: :posts
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 30 }
+  validates :content, presence: true, length: { minimum: 5, maximum: 300 }
+end
